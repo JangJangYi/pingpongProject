e163b811a756b421d95d8cbd8168d67230a7caf1
JangJangYi <jj__yi@imsmobility.co.kr>
2021-11-23 11:07:07 +0900

    Initial commit

---
+++ src/main/java/com/pingpong/pingpongProject/PingpongProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pingpong.pingpongProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpongProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pingpong/pingpongProject/PingpongProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingpong.pingpongProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingpongProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
